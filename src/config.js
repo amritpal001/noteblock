cb064b8ef02647a1eae0ca63fd68232f7c1834f1
amritpal001 <72960084+amritpal001@users.noreply.github.com>
2022-02-03 20:35:02 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   nodes: [{
     identifier: 'NODE-MAIN-2',
     host: 'lavalink-repl.amritpal001.repl.co',
-    password: 'youshallnotpass'
+    password: 'youshallnotpass',
     port: 443,
     secure: true,
   }],
