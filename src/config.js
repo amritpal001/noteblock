236f39b57d0158f365ca037edd11d9265a9f71aa
amritpal001 <72960084+amritpal001@users.noreply.github.com>
2022-02-03 19:52:48 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,11 +2,10 @@ module.exports = {
   owners: [],
   nodes: [{
     identifier: 'NODE-MAIN-2',
-    host: 'lavalink-repl.ahmasa.repl.co',
-    password: 'youshallnotpass',
+    host: 'lavalink-repl.amritpal001.repl.co',
     port: 443,
     secure: true,
   }],
-  prefix: 'nb!',
+  prefix: '-',
   updateSlash: false,
 };
