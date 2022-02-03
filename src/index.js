d0d8c9d7946e9b6712b6d75674dab2fdc5a23bb2
amritpal001 <72960084+amritpal001@users.noreply.github.com>
2022-02-03 20:12:12 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ const NoteClient = require('./Struct/NoteClient');
 
 const Client = new NoteClient();
 
-client.login('NzM4Mzg2NDY2NjM1MzgyNzg1.XyLJ_A.0Delq8jCwjt5qjIq7jveAnQ2Gyk')
 Client.initialize();
