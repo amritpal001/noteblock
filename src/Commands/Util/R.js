d326c2cc9a2d7eaf71a3e28cfebed079c6f54974
amritpal001 <72960084+amritpal001@users.noreply.github.com>
2022-02-10 00:39:18 +0800

    Update R.js

--- src/Commands/Util/R.js
+++ src/Commands/Util/R.js
@@ -1,1 +1,25 @@
 
+const { Command } = require('discord-akairo');
+const { CreateEmbed } = require('../../Utility/CreateEmbed');
+
+module.exports = class RCommand extends Command {
+  constructor() {
+    super('restart', {
+      aliases: ['ping'],
+      description: {
+        content: 'Reload the bot',
+      },
+      category: 'Util',
+      cooldown: 30,
+    });
+  }
+
+  async exec(msg) {
+    try {
+            console.clear();
+               bot.destroy()
+             message.channel.send("Reloaded");
+         return;
+        }
+   }
+};
